0277b27716ffe89ce22dd529650ca9dae0f438d0
LightRider5 <christoph@fulmo.org>
2023-04-03 21:34:11 +0200

    minor bugfix

--- src/Pages/CheckOrder.js
+++ src/Pages/CheckOrder.js
@@ -46,14 +46,15 @@ const CheckOrder = () => {
                 <h4 className="mt-3 order-status" >Status: {orderStatus}</h4>
                 {txhash ? (
                     <div>
+                        <div className='mt-2 order-status'>
+                            <Figure.Image
+                                width={200}
+                                height={200}
+                                alt="Ordinal Preview, if not minted yet"
+                                src={`https://live.ordilabs.org/content/${inscriptionID}`}
 
-                        <Figure.Image
-                            width={200}
-                            height={200}
-                            alt="Ordinal Preview, if not minted yet"
-                            src={`https://live.ordilabs.org/content/${txhash}`}
-                        />
-
+                            />
+                        </div>
                         <h4 className="mt-3 order-status">
                             <a href={`https://mempool.space/de/tx/${txhash}`} target="_blank" rel="noreferrer">
                                 Minting Transaction
