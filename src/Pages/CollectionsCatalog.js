90736e53a345c6e608abfdb266d6cdb7d3938352
LightRider5 <christoph@fulmo.org>
2023-04-04 12:09:07 +0200

    updated collections page

--- src/Pages/CollectionsCatalog.js
+++ src/Pages/CollectionsCatalog.js
@@ -40,22 +40,32 @@ const CollectionsCatalog = () => {
     }
 
     return (
-        <div className="main-middle">
-            <h1>Collections</h1>
-            <Container>
+
+        <div className="collections-catalog">
+            <Container fluid>
+                <h1 className="text-center py-3">Collections</h1>
+                <Form>
+                    <Form.Control
+                        type="search"
+                        placeholder="Search collections"
+                        className="search-input mb-4 mx-auto"
+                        aria-label="Search"
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </Form>
+                <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+                    {currentCollections.map((collection, index) => (
+                        <Col key={index}>
+                            <Link to={`${collection.slug}`} className="collection-link">
+                                <div className="collection-card">
+                                    <CollectionThumbnail collection={collection} />
+                                </div>
+                            </Link>
+                        </Col>
+                    ))}
+                </Row>
                 <Row>
-                    <Col>
-                        <Form>
-                            <Form.Control
-                                type="search"
-                                placeholder="Search collections"
-                                className="m-2 mb-4"
-                                aria-label="Search"
-                                onChange={(e) => setSearch(e.target.value)}
-                            />
-                        </Form>
-                    </Col>
-                    <Col className="d-flex justify-content-end align-items-center">
+                    <Col className="pagination">
                         <Pagination>
                             <Pagination.First onClick={() => paginate(1)} />
                             <Pagination.Prev
@@ -93,103 +103,12 @@ const CollectionsCatalog = () => {
                             <Pagination.Last onClick={() => paginate(pageNumbers.length)} />
                         </Pagination>
                     </Col>
-                </Row>
-                <hr />
-                <Row xs={1} sm={2} md={3} lg={4}>
-                    {currentCollections.map((collection, index) => {
-                        return (
-                            <Link to={`${collection.slug}`} key={index}>
-                                <Col className="m-2 collection-column" key={index}>
-                                    <div key={index}>
-                                        <CollectionThumbnail collection={collection} />
-                                    </div>
-                                </Col>
-                            </Link>
-                        );
-                    })}
+
                 </Row>
             </Container>
         </div>
     );
 };
 
+
 export default CollectionsCatalog;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import { useState } from 'react'
-// import { Container, Row, Col, Form } from 'react-bootstrap'
-// import CollectionThumbnail from '../Components/CollectionThumbnail'
-// import { LinkContainer } from 'react-router-bootstrap'
-// import { collections } from '../data/collections.js'
-// import { Link, Outlet } from 'react-router-dom'
-
-// const CollectionsCatalog = (props) => {
-//     const [search, setSearch] = useState('')
-//     // const [filteredCollections, setFilteredCollections] = useState(collections)
-
-//     const filterCollections = (collections, query) => {
-//         if (!query) {
-//             return collections
-//         }
-//         return collections.filter((collection) =>
-//             collection.name.toLowerCase().includes(query.toLowerCase())
-//         )
-//     }
-//     const filteredCollections = filterCollections(collections, search)
-//     return (
-
-//         <div className='main-middle'>
-//             <h1>Collections</h1>
-//             <Container>
-//                 <Row xs={1} sm={2} md={3} lg={4} id="collection-search-field">
-//                     <Col>
-//                         <Form>
-//                             <Form.Control
-//                                 type="search"
-//                                 placeholder="Search collections"
-//                                 className="m-2 mb-4"
-//                                 aria-label="Search"
-//                                 onChange={(e) => setSearch(e.target.value)}
-//                             />
-//                         </Form>
-//                     </Col>
-//                 </Row>
-//                 <hr />
-//                 <Row xs={2} sm={2} md={3} lg={5}>
-//                     {
-//                         filteredCollections.map((collection, index) => {
-//                             return (
-//                                 <Link to={`${collection.slug}`} key={index}>
-//                                     <Col className='m-2 collection-column' key={index} >
-//                                         <div key={index}>
-//                                             <CollectionThumbnail collection={collection} />
-//                                         </div>
-//                                     </Col>
-//                                 </Link>
-//                             )
-//                         })
-//                     }
-//                 </Row>
-//             </Container>
-
-//         </div >
-
-
-
-
-//     )
-// }
-
-// export default CollectionsCatalog
