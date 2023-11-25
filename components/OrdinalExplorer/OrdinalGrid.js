c7dc348ae27466d4d50f45bef4e66b59865de252
LightRider5 <christoph@fulmo.org>
2023-11-25 12:33:15 +0100

    changed padding for explorer cards

--- components/OrdinalExplorer/OrdinalGrid.js
+++ components/OrdinalExplorer/OrdinalGrid.js
@@ -4,11 +4,11 @@ import OrdinalCard from '../OrdinalCard';
 
 const OrdinalGrid = ({ ordinals }) => {
     return (
-        <Row>
+        <Row className='ordinal-grid-row'>
             {
                 Array.isArray(ordinals) && ordinals.map((inscriptionId, index) => {
                     return (
-                        <Col xs={6} sm={4} md={3} lg={2} xl={2} xxl={1} key={index}>
+                        <Col xs={6} sm={4} md={3} lg={2} xl={2} xxl={1} key={index} className='ordinal-grid-col'>
                             <OrdinalCard ordinalId={inscriptionId} />
                         </Col>
                     );
